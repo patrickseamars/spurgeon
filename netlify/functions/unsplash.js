1d64deb1bc0a221239b929be29503131d045cd04
pseamars <104871011+pseamars@users.noreply.github.com>
2025-01-29 14:12:07 -0500

    Added Netlify function for Unsplash API

---
+++ netlify/functions/unsplash.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+exports.handler = async function () {
+	try {
+		const response = await axios.get('https://api.unsplash.com/photos/random', {
+			params: {
+				query: 'nature,cityscape',
+				client_id: process.env.UNSPLASH_API_KEY // Securely stored in Netlify
+			}
+		})
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify(response.data),
+			headers: {
+				'Access-Control-Allow-Origin': '*', // Allows requests from any domain
+				'Content-Type': 'application/json'
+			}
+		}
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ message: 'Failed to fetch image' })
+		}
+	}
+}
