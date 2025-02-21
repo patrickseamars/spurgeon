44de3a51a2e7cafa63aeccad9ffd934348524ee0
patrickseamars <patrick.seamars@gmail.com>
2025-02-21 14:35:18 -0500

    update orientation

--- netlify/functions/unsplash.js
+++ netlify/functions/unsplash.js
@@ -4,7 +4,8 @@ exports.handler = async function () {
 	try {
 		const response = await axios.get('https://api.unsplash.com/photos/random', {
 			params: {
-				query: 'nature,cityscape',
+				query: 'nature, cityscape',
+				orientation: 'landscape',
 				client_id: process.env.UNSPLASH_API_KEY // Securely stored in Netlify
 			}
 		})
